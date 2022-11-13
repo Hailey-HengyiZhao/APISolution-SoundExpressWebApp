ddd7c73f5cef5e7613e06666a0994e38ea80bf73
Hengyi Zhao <hzhao94@myseneca.ca>
2022-11-13 14:59:02 -0500

    week 9 lab

--- server.js
+++ server.js
@@ -1,84 +1,87 @@
-const express = require("express")
-const app = express()
-const path = require("path")
+const express = require("express");
+const app = express();
+const path = require("path");
 
 const multer = require("multer");
-const cloudinary = require('cloudinary').v2
-const streamifier = require('streamifier')
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
 
-const env = require('dotenv')
-env.config()
+const env = require("dotenv");
+env.config();
 
-const soundService = require("./soundService")
+const soundService = require("./soundService");
 
-const exphbs = require('express-handlebars')
-app.engine('.hbs', exphbs.engine({ extname: '.hbs' }))
-app.set('view engine', '.hbs')
+const exphbs = require("express-handlebars");
+app.engine(".hbs", exphbs.engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
-const HTTP_PORT = process.env.PORT || 8080
+const HTTP_PORT = process.env.PORT || 8080;
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
-  secure: true
-})
+  secure: true,
+});
 
-const upload = multer() // no { storage: storage } 
+const upload = multer(); // no { storage: storage }
 
 function onHttpStart() {
-  console.log("Express http server listening on: " + HTTP_PORT + " 🚀🚀🚀")
+  console.log("Express http server listening on: " + HTTP_PORT + " 🚀🚀🚀");
 }
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   // res.sendFile(path.join(__dirname,"/views/about.html"))
-  res.redirect("/albums")
-})
+  res.redirect("/albums");
+});
 
 app.get("/albums", (req, res) => {
   if (req.query.genre) {
-    soundService.getAlbumsByGenre(req.query.genre).then((genreAlbums) => {
-      res.render('albums', {
-        data: genreAlbums,
-        layout: 'main'
+    soundService
+      .getAlbumsByGenre(req.query.genre)
+      .then((genreAlbums) => {
+        res.render("albums", {
+          data: genreAlbums,
+          layout: "main",
+        });
       })
-    }).catch((err) => res.json({message: err})) 
+      .catch((err) => res.json({ message: err }));
   } else {
-  soundService.getAlbums().then((albums) => {
-    res.render('albums', {
-      data: albums,
-      layout: 'main'
-    })
-  }).catch((err) => res.json({message: err}))
+    soundService
+      .getAlbums()
+      .then((albums) => {
+        res.render("albums", {
+          data: albums,
+          layout: "main",
+        });
+      })
+      .catch((err) => res.json({ message: err }));
   }
-})
-
+});
 
 app.get("/albums/new", (req, res) => {
   // res.sendFile(path.join(__dirname, "/views/albumForm.html"))
   soundService.getGenres().then((genres) => {
-    res.render('albumForm', {
+    res.render("albumForm", {
       data: genres,
-      layout: 'main'
-    })
-  })
-})
+      layout: "main",
+    });
+  });
+});
 
 app.post("/albums/new", upload.single("albumCover"), (req, res) => {
   if (req.file) {
     let streamUpload = (req) => {
       return new Promise((resolve, reject) => {
-        let stream = cloudinary.uploader.upload_stream(
-          (error, result) => {
-            if (result) {
-              resolve(result);
-            } else {
-              reject(error);
-            }
+        let stream = cloudinary.uploader.upload_stream((error, result) => {
+          if (result) {
+            resolve(result);
+          } else {
+            reject(error);
           }
-        );
+        });
 
         streamifier.createReadStream(req.file.buffer).pipe(stream);
       });
@@ -100,43 +103,87 @@ app.post("/albums/new", upload.single("albumCover"), (req, res) => {
   function processPost(imageUrl) {
     req.body.albumCover = imageUrl;
     soundService.addAlbum(req.body).then(() => {
-      res.redirect("/albums")
-    })
+      res.redirect("/albums");
+    });
   }
-
-})
+});
 
 app.get("/albums/:id", (req, res) => {
-  soundService.getAlbumById(req.params.id).then((album) => {
-    var array = []
-    array.push(album)
-    res.render('albums', {
-      data: array,
-      layout: 'main'
+  soundService
+    .getAlbumById(req.params.id)
+    .then((album) => {
+      // var array = []
+      // array.push(album)
+      res.render("albums", {
+        data: array,
+        layout: "main",
+      });
     })
-  }).catch((err) => {
-    res.json({message: err})
-  })
-})
+    .catch((err) => {
+      res.json({ message: err });
+    });
+});
+
+app.get("/albums/delete/:id", (req, res) => {
+  soundService
+    .deleteAlbum(req.params.id)
+    .then(() => {
+      res.redirect("/albums");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.get("/genres", (req, res) => {
-  soundService.getGenres().then((genres) => {
-    res.render('genres', {
-      data: genres,
-      layout: 'main'
+  soundService
+    .getGenres()
+    .then((genres) => {
+      res.render("genres", {
+        data: genres,
+        layout: "main",
+      });
     })
-  }).catch((err) => {
-    console.log(err)
-    res.json({message: err})
-  })
-})
+    .catch((err) => {
+      console.log(err);
+      res.json({ message: err });
+    });
+});
+
+app.get("/genres/new", (req, res) => {
+  soundService
+    .getGenres()
+    .then((genres) => {
+      res.render("genreForm", {
+        layout: "main",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ message: err });
+    });
+});
+
+app.get("/genres/delete/:id", (req, res) => {
+  soundService
+    .deleteGenre(req.params.id)
+    .then(() => {
+      res.redirect("/genres");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.use((req, res) => {
   res.status(404).send("Page Not Found");
-})
+});
 
-soundService.initialize().then(() => {
-  app.listen(HTTP_PORT, onHttpStart)
-}).catch((err) => {
-  console.log(err)
-})
+soundService
+  .initialize()
+  .then(() => {
+    app.listen(HTTP_PORT, onHttpStart);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- soundService.js
+++ soundService.js
@@ -1,89 +1,203 @@
+const fs = require("fs");
+const { title } = require("process");
+const Sequelize = require("sequelize");
+var sequelize = new Sequelize(
+  "mxhagusa",
+  "mxhagusa",
+  "RcTbStCUZ0o9lp5bBZYvSoS0eGFuX8RL",
+  {
+    host: "peanut.db.elephantsql.com",
+    dialect: "postgres",
+    port: 5432,
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false },
+    },
+    query: { raw: true },
+  }
+);
 
-const fs = require("fs")
+var Album = sequelize.define("Album", {
+  albumID: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: Sequelize.STRING,
+  artist: Sequelize.STRING,
+  albumCover: Sequelize.STRING,
+  year: Sequelize.INTEGER,
+});
 
+var Genre = sequelize.define("Genre", {
+  genreID: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  genre: Sequelize.STRING,
+});
 
-let albums = []
-let genres = []
+Album.belongsTo(Genre, { foreignKey: "genreID" });
+
+sequelize
+  .authenticate()
+  .then(function () {
+    console.log("Connection has been established successfully.");
+  })
+  .catch(function (err) {
+    console.log("Unable to connect to the database:", err);
+  });
 
 module.exports.initialize = () => {
   return new Promise((resolve, reject) => {
-    fs.readFile('./data/albums.json', 'utf8', (err, data) => {
-      if (err) {
-        reject(err)
-      } else {
-        // console.log(data)
-        albums = JSON.parse(data)
-        fs.readFile('./data/genres.json', 'utf8', (err, data) => {
-          if (err) {
-            reject(err)
-          } else {
-            // console.log(data)
-            genres = JSON.parse(data)
-            resolve("Success!")
-          }
-        })
-      }
-    })
-  })
-}
+    // fs.readFile("./data/albums.json", "utf8", (err, data) => {
+    //   if (err) {
+    //     reject(err);
+    //   } else {
+    //     // console.log(data)
+    //     albums = JSON.parse(data);
+    //     fs.readFile("./data/genres.json", "utf8", (err, data) => {
+    //       if (err) {
+    //         reject(err);
+    //       } else {
+    //         // console.log(data)
+    //         genres = JSON.parse(data);
+    //         resolve("Success!");
+    //       }
+    //     });
+    //   }
+    // });
+
+    sequelize
+      .sync()
+      .then(() => {
+        console.log("Database sync completed");
+        resolve();
+      })
+      .catch(() => {
+        console.log("Database sync failed");
+        reject();
+      });
+  });
+};
 
 module.exports.getAlbums = () => {
   return new Promise((resolve, reject) => {
-    if(albums.length > 0) {
-      resolve(albums)
-    } else {
-      reject("no albums")
-    }
-  })
-}
+    Album.findAll()
+      .then((albumData) => {
+        resolve(albumData);
+      })
+      .catch((err) => {
+        console.log("CAN'T FIND ALBUM! ERROR" + err);
+        reject();
+      });
+  });
+};
 
 module.exports.getAlbumById = (id) => {
   return new Promise((resolve, reject) => {
-    for(let i = 0; i < albums.length; i++) {
-      var album;
-      if (albums[i].id == id) {
-        album = albums[i]
-      }
-    }
-
-    if (album) {
-      resolve(album)
-    } else {
-      reject("Album not found!")
-    }
-  })
-}
+    Album.findAll({
+      where: {
+        albumID: id,
+      },
+    })
+      .then((genreData) => {
+        resolve(genreData);
+      })
+      .catch((err) => {
+        console.log("CAN'T FIND GENRES! ERROR" + err);
+        reject();
+      });
+  });
+};
 
 module.exports.getGenres = () => {
   return new Promise((resolve, reject) => {
-    if(genres.length > 0) {
-      resolve(genres)
-    } else {
-      reject("no albums")
-    }
-  })
-}
+    Genre.findAll()
+      .then((genreData) => {
+        resolve(genreData);
+      })
+      .catch((err) => {
+        console.log("CAN'T FIND GENRES! ERROR" + err);
+        reject();
+      });
+  });
+};
+
+module.exports.addGenre = (genre) => {
+  return new Promise((resolve, reject) => {
+    Genre.create(genre)
+      .then(() => {
+        console.log("Genre created");
+        resolve();
+      })
+      .catch((err) => {
+        console.log(err);
+        reject();
+      });
+  });
+};
 
 module.exports.addAlbum = (album) => {
   return new Promise((resolve, reject) => {
-      album.id = albums.length + 1
-      albums.push(album)
-      resolve()
-  })
-}
+    Album.create(album)
+      .then(() => {
+        console.log("Album created");
+        resolve();
+      })
+      .catch((err) => {
+        console.log(err);
+        reject();
+      });
+  });
+};
 
 module.exports.getAlbumsByGenre = (genreID) => {
   return new Promise((resolve, reject) => {
-    var genreAlbums = []
-    for(let i = 0; i < albums.length; i++) {
-      if (albums[i].genre == genreID) {
-        genreAlbums.push(albums[i])
-      }
-    }
-    if (genreAlbums) {
-      resolve(genreAlbums)
-    } else {
-      reject("Albums not found for this genre!")
-    }
-  })
-}
+    Album.findAll({
+      where: {
+        genreID: genreID,
+      },
+    })
+      .then((albums) => {
+        resolve(albums);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
+
+module.exports.deleteGenre = (genreID) => {
+  return new Promise((resolve, reject) => {
+    Genre.destroy({
+      where: {
+        genreID: genreID,
+      },
+    })
+      .then(() => {
+        console.log("Genre deleted");
+        resolve();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
+
+module.exports.deleteAlbum = (albumID) => {
+  return new Promise((resolve, reject) => {
+    Album.destroy({
+      where: {
+        albumID: albumID,
+      },
+    })
+      .then(() => {
+        console.log("Album deleted");
+        resolve();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
